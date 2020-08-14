efc5bcc8501580a11e86a441427b93ca2f46d135
Vishnu Dileesh <dev@vishnudileesh.com>
2020-08-14 17:24:08 +0530

    Install & setup ngxagora package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxAgoraModule, AgoraConfig } from 'ngx-agora';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClassRoomComponent } from './pages/class-room/class-room.component';
 
+import { environment } from '../environments/environment.prod';
+
+const agoraConfig: AgoraConfig = {
+  AppID: environment.AGORA_APPID,
+};
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    ClassRoomComponent
-  ],
+  declarations: [AppComponent, ClassRoomComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxAgoraModule.forRoot(agoraConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
