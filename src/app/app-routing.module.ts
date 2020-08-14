f091c14498ec96749e4646331d67307db3b82bfd
Vishnu Dileesh <dev@vishnudileesh.com>
2020-08-14 17:03:49 +0530

    Add classroom class component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ClassRoomComponent } from './pages/class-room/class-room.component';
+
+const routes: Routes = [
+  { path: 'classroom/10c', component: ClassRoomComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassRoomComponent } from './pages/class-room/class-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassRoomComponent
   ],
   imports: [
     BrowserModule,
